d1ae8dc46ac4bc5aba5e8b7487b9aedd937a9721
Yeli <yeli>
2024-03-21 16:51:00 +0100

    probando

--- src/main.c
+++ src/main.c
@@ -6,42 +6,40 @@
 /*   By: eerazo-c <eerazo-c@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:27:38 by eerazo-c          #+#    #+#             */
-/*   Updated: 2024/03/20 13:35:33 by eerazo-c         ###   ########.fr       */
+/*   Updated: 2024/03/21 16:45:54 by eerazo-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Inc/fractol.h"
 
-int check(int ac, char **av)
+int	check(int ac, char **av)
 {
 	if (ac == 1)
 	{
 		write(1, "The arguments are not correct\n", 30);
 		return (0);
 	}
-	else if (ft_strncmp(*av, "mandelbrot", 10) != '\0'
-			&& ft_strncmp(*av, "julia", 5) != '\0')
+	else if (ft_strncmp(*av, "mandelbrot", 10) != '\0' && ft_strncmp(*av,
+			"julia", 5) != '\0')
 	{
 		write(1, "Avaliable fractals:\n", 18);
 		write(1, "- Mandelbrot\n", 13);
 		write(1, "- Julia\n", 8);
-		return(0);
+		return (0);
 	}
 	return (1);
 }
 
-int main (int ac, char **av)
+int	main(int ac, char **av)
 {
 	t_plano	f;
 
-	ft_printf("probando, si funciona");	
+	ft_printf("probando, si funciona");
 	if (!check(ac, &av[1]))
 		exit(1);
 	f.name = av[1];
 	fractal_initit(&f);
 	start_render(&f);
 	mlx_loop(f.mlx);
-	
 	return (EXIT_FAILURE);
 }
-
